ee1a6da4dc1b3484ceff35e0f1033620f2e73d34
ruben <rubiosax52@gmail.com>
2025-11-03 14:33:17 +0100

    Ejercicios terminados

--- EjerciciosJavaScrip/Ejercicio5.02/biblioteca/ejercicio1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const titulos = document.getElementsByClassName("acordeon-titulo");
-
-const alternarVista = (elemento) => {
-    elemento.classList.toggle("ocultar");
-}
-
-export { alternarVista };
