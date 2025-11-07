40416841f48e8da3513f0989772ec0e2a8455fcb
ruben <rubiosax52@gmail.com>
2025-11-07 18:50:15 +0100

    Haciendo cositas

--- EjerciciosJavaScrip/Ejercicio5.04/biblioteca/ejercicio1.js
+++ EjerciciosJavaScrip/Ejercicio5.04/biblioteca/ejercicio1.js
@@ -18,4 +18,22 @@ const crearPanel = (arrayImagenes) => {
 	document.getElementById("piezas").appendChild(lista);
 };
 
-export { imagenesAleatorio, crearPanel };
+const construirTablero = (alto, ancho) => {
+	const tabla = document.createElement("table");
+	for (let i = 0; i < alto; i++) {
+		const fila = document.createElement("tr");
+		for (let j = 0; j < ancho; j++) {
+			const celda = document.createElement("td");
+			fila.appendChild(celda);
+		}
+		tabla.appendChild(fila);
+	}
+	document.getElementById("tablero").appendChild(tabla);
+};
+
+const reiniciar = (panel, tablero) => {
+	panel.innerHTML = "";
+	tablero.innerHTML = "";
+};
+
+export { imagenesAleatorio, crearPanel, construirTablero, reiniciar };
--- EjerciciosJavaScrip/Ejercicio5.04/main.js
+++ EjerciciosJavaScrip/Ejercicio5.04/main.js
@@ -1,19 +1,35 @@
 "use strict";
 
-import { imagenesAleatorio, crearPanel } from "./biblioteca/ejercicio1.js";
+import {
+	imagenesAleatorio,
+	crearPanel,
+	construirTablero,
+	reiniciar,
+} from "./biblioteca/ejercicio1.js";
 
-let imagenes = [
-	"./images/1-1.jpg",
-	"./images/1-2.jpg",
-	"./images/1-3.jpg",
-	"./images/2-1.jpg",
-	"./images/2-2.jpg",
-	"./images/2-3.jpg",
-	"./images/3-1.jpg",
-	"./images/3-2.jpg",
-	"./images/3-3.jpg",
-];
+window.onload = () => {
+	const panel = document.getElementById("piezas");
+	const tablero = document.getElementById("tablero");
+	const reiniciarBtn = document.getElementById("reiniciar");
 
-let imagenesRandomizadas = imagenesAleatorio(imagenes);
+	let imagenes = [
+		"./images/1-1.jpg",
+		"./images/1-2.jpg",
+		"./images/1-3.jpg",
+		"./images/2-1.jpg",
+		"./images/2-2.jpg",
+		"./images/2-3.jpg",
+		"./images/3-1.jpg",
+		"./images/3-2.jpg",
+		"./images/3-3.jpg",
+	];
 
-crearPanel(imagenesRandomizadas);
+	crearPanel(imagenesAleatorio(imagenes));
+	construirTablero(3, 3);
+
+	reiniciarBtn.addEventListener("click", () => {
+		reiniciar(panel, tablero);
+		crearPanel(imagenesAleatorio(imagenes));
+		construirTablero(3, 3);
+	});
+}; // Fin windows.onload.
