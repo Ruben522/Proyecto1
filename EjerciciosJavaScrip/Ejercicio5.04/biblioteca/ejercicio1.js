f95480f18c6b06ac32a623f53c314084743ae049
ruben <rubiosax52@gmail.com>
2025-11-10 19:38:53 +0100

    Jercicio de js 5.04 puzzle terminado

--- EjerciciosJavaScrip/Ejercicio5.04/biblioteca/ejercicio1.js
+++ EjerciciosJavaScrip/Ejercicio5.04/biblioteca/ejercicio1.js
@@ -1,61 +1,89 @@
 "use strict";
 
+// Retorna una copia randomizada del array de imágenes original.
 const imagenesAleatorio = (imagenes) => {
-    let copia = [...imagenes];
-    copia.sort(() => Math.random() - 0.5);
-    return copia;
+	let copia = [...imagenes];
+	copia.sort(() => Math.random() - 0.5);
+	return copia;
 };
 
+// Crea el panel con las imágenes del puzzle.
 const crearPanel = (arrayImagenes) => {
-    const panel = document.getElementById("piezas");
-    for (let i = 0; i < arrayImagenes.length; i++) {
-        let img = document.createElement("img");
-        img.src = arrayImagenes[i];
-        img.classList.add("arrastable");
-        img.id = "pieza-" + i;
-        panel.appendChild(img);
-    }
+	const panel = document.getElementById("piezas");
+	for (let i = 0; i < arrayImagenes.length; i++) {
+		let img = document.createElement("img");
+		img.id = arrayImagenes[i];
+		img.src = arrayImagenes[i];
+		// Añadimos a cada imagen este atributo para poder hacer la comprobación
+		// en la función tableroResuelto().
+		img.setAttribute("ruta-original", arrayImagenes[i]);
+		img.classList.add("arrastable");
+		img.setAttribute("draggable", true);
+		panel.appendChild(img);
+	}
 };
 
-const construirTablero = (alto, ancho) => {
-    const tablero = document.getElementById("tablero");
-    const tabla = document.createElement("table");
-    for (let i = 0; i < alto; i++) {
-        const fila = document.createElement("tr");
-        for (let j = 0; j < ancho; j++) {
-            const celda = document.createElement("td");
-            celda.classList.add("soltable");
-            fila.appendChild(celda);
-        }
-        tabla.appendChild(fila);
-    }
-    tablero.appendChild(tabla);
+// Crea el tablero de juego.
+const construirTablero = (imagenes) => {
+	const tablero = document.getElementById("tablero");
+	const tabla = document.createElement("table");
+	let k = 0;
+	for (let i = 0; i < 3; i++) {
+		const fila = document.createElement("tr");
+		for (let j = 0; j < 3; j++) {
+			const celda = document.createElement("td");
+			celda.classList.add("soltable");
+			// Añadimos a cada celda este atributo para poder hacer la comprobación
+			// en la función tableroResuelto().
+			celda.setAttribute("ruta-original", imagenes[k]);
+			fila.appendChild(celda);
+			k++;
+		}
+		tabla.appendChild(fila);
+	}
+	tablero.appendChild(tabla);
 };
 
+// Elimina el contenido.
 const reiniciar = (panel, tablero) => {
-    panel.innerHTML = "";
-    tablero.innerHTML = "";
-    const mensaje = document.getElementById("mensaje");
-    mensaje.style.display = "none";
+	panel.innerHTML = "";
+	tablero.innerHTML = "";
+	let mensaje = document.getElementById("mensaje");
+	mensaje.style.display = "none";
 };
 
-const dragAndDrop = () => {
-    const piezas = document.getElementsByClassName("arrastable");
-    for (let i = 0; i < piezas.length; i++) {
-        piezas[i].setAttribute("draggable", true);
-    }
+// Cambia la clase CSS del mensaje.
+const mostrarMensaje = () => {
+	let mensaje = document.getElementById("mensaje");
+	mensaje.style.display = "block";
 };
 
-const mensajePuzzleCompletado = () => {
-    const mensaje = document.getElementById("mensaje");
-    mensaje.style.display = "block";
-};
+// Comprueba si el tablero ha sido resuelto.
+// Cuando se resuelve, llama a la función
+const tableroResuelto = () => {
+	let celdas = document.getElementsByClassName("soltable");
+	let acertado = 0;
 
+	for (let i = 0; i < celdas.length; i++) {
+		let celda = celdas[i];
+		let imagenDentro = celda.querySelector("img");
+		if (imagenDentro) {
+			let idCorrecto = celda.getAttribute("ruta-original");
+			let idPieza = imagenDentro.getAttribute("ruta-original");
+
+			if (idCorrecto === idPieza) {
+				acertado++;
+			}
+		}
+	}
+	if (acertado === celdas.length) {
+		mostrarMensaje();
+	}
+};
 export {
-    imagenesAleatorio,
-    crearPanel,
-    construirTablero,
-    reiniciar,
-    dragAndDrop,
-    mensajePuzzleCompletado
+	imagenesAleatorio,
+	crearPanel,
+	construirTablero,
+	reiniciar,
+	tableroResuelto,
 };
--- EjerciciosJavaScrip/Ejercicio5.04/main.js
+++ EjerciciosJavaScrip/Ejercicio5.04/main.js
@@ -1,80 +1,79 @@
 "use strict";
 
 import {
-    imagenesAleatorio,
-    crearPanel,
-    construirTablero,
-    reiniciar,
-    dragAndDrop,
+	imagenesAleatorio,
+	crearPanel,
+	construirTablero,
+	reiniciar,
+	tableroResuelto,
 } from "./biblioteca/ejercicio1.js";
 
 window.onload = () => {
-    const panel = document.getElementById("piezas");
-    const tablero = document.getElementById("tablero");
-    const reiniciarBtn = document.getElementById("reiniciar");
+	const panel = document.getElementById("piezas");
+	const tablero = document.getElementById("tablero");
+	const reiniciarBtn = document.getElementById("reiniciar");
 
-    let imagenes = [
-        "./images/1-1.jpg",
-        "./images/1-2.jpg",
-        "./images/1-3.jpg",
-        "./images/2-1.jpg",
-        "./images/2-2.jpg",
-        "./images/2-3.jpg",
-        "./images/3-1.jpg",
-        "./images/3-2.jpg",
-        "./images/3-3.jpg",
-    ];
+	let imagenes = [
+		"./images/1-1.jpg",
+		"./images/1-2.jpg",
+		"./images/1-3.jpg",
+		"./images/2-1.jpg",
+		"./images/2-2.jpg",
+		"./images/2-3.jpg",
+		"./images/3-1.jpg",
+		"./images/3-2.jpg",
+		"./images/3-3.jpg",
+	];
 
-    crearPanel(imagenesAleatorio(imagenes));
-    construirTablero(3, 3);
-    dragAndDrop();
+	crearPanel(imagenesAleatorio(imagenes));
+	construirTablero(imagenes);
 
-    panel.addEventListener("dragstart", (evento) => {
-        if (evento.target.classList.contains("arrastable")) {
-            evento.dataTransfer.setData("id", evento.target.id);
-        }
-    });
+	panel.addEventListener("dragstart", (evento) => {
+		if (evento.target.classList.contains("arrastable")) {
+			evento.dataTransfer.setData("id", evento.target.id);
+		}
+	});
 
-    panel.addEventListener("dragover", (evento) => {
-        evento.preventDefault();
-    });
+	panel.addEventListener("dragover", (evento) => {
+		evento.preventDefault();
+	});
 
-    panel.addEventListener("drop", (evento) => {
-        evento.preventDefault();
-        const id = evento.dataTransfer.getData("id");
-        const pieza = document.getElementById(id);
-        if (pieza && pieza.classList.contains("arrastable")) {
-            panel.appendChild(pieza);
-        }
-    });
+	panel.addEventListener("drop", (evento) => {
+		evento.preventDefault();
+		let id = evento.dataTransfer.getData("id");
+		let pieza = document.getElementById(id);
+		if (pieza && pieza.classList.contains("arrastable")) {
+			panel.appendChild(pieza);
+		}
+	});
 
-    tablero.addEventListener("dragstart", (evento) => {
-        if (evento.target.classList.contains("arrastable")) {
-            evento.dataTransfer.setData("id", evento.target.id);
-        }
-    });
+	tablero.addEventListener("dragstart", (evento) => {
+		if (evento.target.classList.contains("arrastable")) {
+			evento.dataTransfer.setData("id", evento.target.id);
+		}
+	});
 
-    tablero.addEventListener("dragover", (evento) => {
-        evento.preventDefault();
-    });
+	tablero.addEventListener("dragover", (evento) => {
+		evento.preventDefault();
+	});
 
-    tablero.addEventListener("drop", (evento) => {
-        evento.preventDefault();
-        const id = evento.dataTransfer.getData("id");
-        const pieza = document.getElementById(id);
-        if (
-            pieza &&
-            evento.target.classList.contains("soltable") &&
-            evento.target.children.length === 0
-        ) {
-            evento.target.appendChild(pieza);
-        }
-    });
+	tablero.addEventListener("drop", (evento) => {
+		evento.preventDefault();
+		let id = evento.dataTransfer.getData("id");
+		let pieza = document.getElementById(id);
+		if (
+			pieza &&
+			evento.target.classList.contains("soltable") &&
+			evento.target.children.length === 0
+		) {
+			evento.target.appendChild(pieza);
+			tableroResuelto();
+		}
+	});
 
-    reiniciarBtn.addEventListener("click", () => {
-        reiniciar(panel, tablero);
-        crearPanel(imagenesAleatorio(imagenes));
-        construirTablero(3, 3);
-        dragAndDrop();
-    });
-};
+	reiniciarBtn.addEventListener("click", () => {
+		reiniciar(panel, tablero);
+		crearPanel(imagenesAleatorio(imagenes));
+		construirTablero(imagenes);
+	});
+}; //Fin windows.onload.
