e6781f1662d274a3a705ba488c5104e23523b1d0
ruben <rubiosax52@gmail.com>
2025-11-20 18:53:54 +0100

    Empezando el ejercicio 5.06 de react

---
+++ EjercicioReact/Ejercicio5.06/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Menu from "./components/Menu";
+import Contenedor from "./components/Contenedor";
+import Cabecera from "./components/Cabecera";
+import Contenido from "./components/Contenido";
+import Footer from "./components/Footer";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Contenedor>
+				<Cabecera />
+				<Menu />
+				<Contenido />
+				<Footer />
+			</Contenedor>
+		</>
+	);
+}
+
+export default App;
---
+++ EjercicioReact/Ejercicio5.06/src/routes/Rutas.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Inicio from "../pages/Inicio";
+import Error from "../pages/Error";
+
+// Componente de rutas de la aplicación.
+const Rutas = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Inicio />} />
+			<Route path="/insertar-disco" element={<InsertarDisco />} />
+			/* Ruta para manejar errores en caso de que no se encuentre la página */
+			<Route path="*" element={<Error />} />
+		</Routes>
+	);
+};
+
+export default Rutas;
